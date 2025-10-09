38d5e86d8a45aa0f9cb323bf77ab9d6b5a85c358
lariany duarte <lariany.martins@aluno.ce.gov.br>
2025-10-09 17:44:23 +0000

    resolvido

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,18 @@
 // CRIE UMA LÓGICA AQUI
+ let numero= 1
+ while(numero>=10) {
+    console.log(`contagem ${numero} `)
+    numero ++
+ }
+ let resultado=10
+
+
+
+
+
+
+
+
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
